8de420e1eae1c9800764092ace0b57d3eed68b81
Karl Watson <karlwatson09@gmail.com>
2017-04-17 08:38:16 -0700

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,14 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
 
   def show
      @user = User.find(params[:id])
+      @microposts = @user.microposts.paginate(page: params[:page])
     #  debugger
   end
 
@@ -14,12 +21,28 @@ class UsersController < ApplicationController
    if @user.save
      log_in @user
      remember @user
-      flash[:success] = "Welcome to the Sample App!"
+     flash[:success] = "Welcome to the Sample App!"
      # Handle a successful save.
      redirect_to user_url(@user)
    else
      render 'new'
    end
+ end
+
+ def edit
+ end
+
+ def update
+   if @user.update_attributes(user_params)
+     flash[:success] = "Profile updated"
+     redirect_to @user
+   else
+     render 'edit'
+   end
+ end
+
+ def feed
+   Micropost.where("user_id = ?", id)
  end
 
  private
@@ -29,4 +52,13 @@ class UsersController < ApplicationController
                                   :password_confirmation)
    end
 
+   # Before filters
+
+
+
+   # Confirms the correct user.
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
 end
